eabbe967e4a27833b40bebe451a794a7afd3c322
gowthamr30 <14p138@skct.edu.in>
2017-02-12 22:20:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,63 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var book= {
+    title:'book section',
+    heading:'what type of book you want',
+    date:'feb 12',
+    content:` <p>
+                 love yoursef and make others to lovelove yoursef and make others to lovelove yoursef and make others to lovelove yoursef and make others to lovelove yoursef and make others to love
+             </p>
+              <p>
+                 love yoursef and make others to lovelove yoursef and make others to lovelove yoursef and make others to lovelove yoursef and make others to lovelove yoursef and make others to love
+             </p>
+              <p>
+                 love yoursef and make others to lovelove yoursef and make others to lovelove yoursef and make others to lovelove yoursef and make others to lovelove yoursef and make others to love
+             </p>`
+
+};
+function createtemplate (data) {
+var title= data.title;
+var heading= data.heading;
+var date= data.date;
+var content= data.content;
+var htmltempalte = `
+ <html>
+ <head>
+     <title>
+         ${title} 
+     </title>
+     <meta name="viewport" content="width=device-width, inital=scale-1" />
+     <link href="/ui/style.css" rel="stylesheet" />
+ </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+             <hr/>
+            <h2>
+                ${heading}
+            </h2>
+            <div>
+                ${date}
+            </div>
+            <div>
+             ${content}
+             </div>
+        </div>
+    </body>
+</html>
+
+`;
+return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/book', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'book.html'));
+   res.send(createtemplate(book));
 });
 
 app.get('/electronics', function (req, res) {
