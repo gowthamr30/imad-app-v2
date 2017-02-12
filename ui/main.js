5f2173b693cf021c3c79c42efd1ba3181967ab6b
gowthamr30 <14p138@skct.edu.in>
2017-02-12 23:56:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ function moveRight () {
     marginLeft=marginLeft+1;
     img.style.marginLeft= marginLeft + 'px';
 }
-img.onclick=function(){
+//img.onclick=function(){
   //img.style.marginLeft='100px';
-  var interval=setInterval(moveRight,50);
-}
+// var interval=setInterval(moveRight,50);
+//}
